38b9adf4c1eca4c41be9af7f1064edd439e9a5ec
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-03-02 10:11:53 +0100

    Set up connection to db

--- DataAccess/Data/ApplicationDbContext.cs
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Models.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,10 @@ namespace DataAccess.Data
         {
 
         }
-     
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Engine> Engines { get; set; }
+        public DbSet<EngineType> EngineTypes { get; set; }
+
+
     }
 }
