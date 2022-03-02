93b417c7c2df4e35e1f144d61b7de3a27bbc4fc3
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-03-02 10:05:34 +0100

    Created Models (tables for database)

---
+++ Models/Models/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class Engine
+    {
+        [Key]
+        public int Engine_Id { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public int SerialNumber { get; set; }
+        [Required]
+        public string Type { get; set; }
+    }
+}
